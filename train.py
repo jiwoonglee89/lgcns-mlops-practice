2734b65a8e453b3f18e894c34dd20036730b28a2
jiwoonglee89 <leewo1001@gmail.com>
2023-08-25 01:46:38 +0000

    Add logger into train.py

--- train.py
+++ train.py
@@ -24,22 +24,26 @@ from src.common.utils import get_param_set
 from src.preprocess import preprocess_pipeline
 
 # 로그 들어갈 위치
-#  로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+# 로그를 정해진 로그 경로에 logs.log로 저장하도록 설정
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
 
 sys.excepthook = handle_exception
 warnings.filterwarnings(action="ignore")
 
 
 if __name__ == "__main__":
+    logger.info("Loading data...")
     train_df = pd.read_csv(os.path.join(DATA_PATH, "house_rent_train.csv"))
 
     _X = train_df.drop(["rent", "area_locality", "posted_on"], axis=1)
     y = np.log1p(train_df["rent"])
 
     #  X=_X, y=y로 전처리 파이프라인을 적용해 X에 저장
+    logger.info("Applying a pipeline")
     X = preprocess_pipeline.fit_transform(X=_X, y=y)
 
     # Data storage - 피처 데이터 저장
+    logger.info("Saving a featrue Data...")
     if not os.path.exists(os.path.join(DATA_PATH, "storage")):
         os.makedirs(os.path.join(DATA_PATH, "storage"))
 
@@ -62,6 +66,7 @@ if __name__ == "__main__":
     mlflow.set_experiment(experiment_name=experiment_name)
     mlflow.set_tracking_uri("./mlruns")
 
+    logger.debug("Using mlflow")
     for i, params in enumerate(param_set):
         run_name = f"Run {i}"
         with mlflow.start_run(run_name=f"Run {i}"):
@@ -88,6 +93,7 @@ if __name__ == "__main__":
                     "RMSE_CV": score_cv.mean()  # RMSE_CV 라는 이름으로 score_cv.mean()을 저장
                 }
             )
+            logger.info(f"RMSE CV FOR RUN {i}:{score_cv.mean()}")
 
             # 로깅 정보 : 학습 loss
             for s in regr.train_score_:
@@ -119,6 +125,7 @@ if __name__ == "__main__":
 
     best_run = mlflow.get_run(best_run_df.at[0, "run_id"])
     best_params = best_run.data.params
+    logger.info(f"Best hyper-parameter:{best_params}")
 
     best_model_uri = f"{best_run.info.artifact_uri}/model"
 
